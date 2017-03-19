3e10858e6b7943f153f6f9948bfc19f141df6676
wareya <wareya@gmail.com>
2017-03-19 15:58:06 -0400

    Code cleaning and add a headword blacklist

--- readme.md
+++ readme.md
@@ -3,5 +3,5 @@
 generates a word frequency list from japanese utf-8 text  
 depends on kuromoji-unidic-kanaaccent from maven  
 invoke java -jar analyzer.jar mycorpus.txt > myfrequencylist.txt  
-licensed under the same license as kuromoji for now  
+licensed under a public domain–like permissive license  
 particles, auxiliary verbs, etc are blacklisted from output
--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,6 @@ import com.atilika.kuromoji.unidic.kanaaccent.Tokenizer;
 import com.atilika.kuromoji.util.StringUtils;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -25,15 +24,166 @@ import java.util.regex.Pattern;
 
 public class Main
 {
-    static HashSet<String> blacklist;
+    // to force utf-8 output on windows
+    private static PrintStream out;
+
+    // disable to make "academic" frequency lists (keep enabled for flashcards)
+    private static boolean blacklist_enabled = true;
+    private static HashSet<String> blacklist = new HashSet<>();
+
     private static void init_blacklist()
     {
+    // weekdays
+        blacklist.add("日曜");
+        blacklist.add("月曜");
+        blacklist.add("火曜");
+        blacklist.add("水曜");
+        blacklist.add("木曜");
+        blacklist.add("金曜");
+        blacklist.add("土曜");
+        blacklist.add("日曜日");
+        blacklist.add("月曜日");
+        blacklist.add("火曜日");
+        blacklist.add("水曜日");
+        blacklist.add("木曜日");
+        blacklist.add("金曜日");
+        blacklist.add("土曜日");
+    // months
+        blacklist.add("一月");
+        blacklist.add("二月");
+        blacklist.add("三月");
+        blacklist.add("四月");
+        blacklist.add("五月");
+        blacklist.add("六月");
+        blacklist.add("七月");
+        blacklist.add("八月");
+        blacklist.add("九月");
+        blacklist.add("十月");
+        blacklist.add("十一月");
+        blacklist.add("十二月");
+    // monthdays
+        blacklist.add("一日");
+        blacklist.add("二日");
+        blacklist.add("三日");
+        blacklist.add("四日");
+        blacklist.add("五日");
+        blacklist.add("六日");
+        blacklist.add("七日");
+        blacklist.add("八日");
+        blacklist.add("九日");
+        blacklist.add("十日");
+        blacklist.add("十一日");
+        blacklist.add("十二日");
+        blacklist.add("十三日");
+        blacklist.add("十四日");
+        blacklist.add("十五日");
+        blacklist.add("十六日");
+        blacklist.add("十七日");
+        blacklist.add("十八日");
+        blacklist.add("十九日");
+        blacklist.add("二十日");
+        blacklist.add("二十一日");
+        blacklist.add("二十二日");
+        blacklist.add("二十三日");
+        blacklist.add("二十四日");
+        blacklist.add("二十五日");
+        blacklist.add("二十六日");
+        blacklist.add("二十七日");
+        blacklist.add("二十八日");
+        blacklist.add("二十九日");
+        blacklist.add("三十 日");
+        blacklist.add("三十一日");
+    // numbers
+        blacklist.add("0");
+        blacklist.add("1");
+        blacklist.add("2");
+        blacklist.add("3");
+        blacklist.add("4");
+        blacklist.add("5");
+        blacklist.add("6");
+        blacklist.add("7");
+        blacklist.add("8");
+        blacklist.add("9");
+        blacklist.add("０");
+        blacklist.add("１");
+        blacklist.add("２");
+        blacklist.add("３");
+        blacklist.add("４");
+        blacklist.add("５");
+        blacklist.add("６");
+        blacklist.add("７");
+        blacklist.add("８");
+        blacklist.add("９");
+        blacklist.add("〇");
+        blacklist.add("一");
+        blacklist.add("二");
+        blacklist.add("三");
+        blacklist.add("四");
+        blacklist.add("五");
+        blacklist.add("六");
+        blacklist.add("七");
+        blacklist.add("八");
+        blacklist.add("九");
+        blacklist.add("十"); // 10
+        blacklist.add("百"); // 100
+        blacklist.add("千"); // 1000
+        blacklist.add("万"); // 10000 (myriad)
+        blacklist.add("億"); // myriad^2
+        blacklist.add("兆"); // myriad^3
+        blacklist.add("京"); // myriad^4
+        blacklist.add("垓"); // myriad^5
+    // basic counters
+        blacklist.add("一つ");
+        blacklist.add("二つ");
+        blacklist.add("三つ");
+        blacklist.add("四つ");
+        blacklist.add("五つ");
+        blacklist.add("六つ");
+        blacklist.add("七つ");
+        blacklist.add("八つ");
+        blacklist.add("九つ");
+    // VN-specific names
+        // ingarock
         blacklist.add("ギー");
+        blacklist.add("ドクター");
+        // magicha
+        blacklist.add("謳歌");
+        // fate
+        blacklist.add("セイバー");
+        blacklist.add("マスター");
+        blacklist.add("キャスター");
+        blacklist.add("ライダー");
+        blacklist.add("ランサー");
+
+        // hoshimemo
+        blacklist.add("ヒバリ");
+    // jargon
+        blacklist.add("スクワッター");
+        blacklist.add("聖杯");
+        blacklist.add("機関");
+        blacklist.add("望遠");
+    // parsing mysteries
+        blacklist.add("チャー");
+        blacklist.add("師");
+        blacklist.add("洋");
+        blacklist.add("師");
+        blacklist.add("崎");
+    // special particles
+        blacklist.add("お");
+        blacklist.add("ご");
+        blacklist.add("御");
+        blacklist.add("さん");
+        blacklist.add("ちゃん");
+        blacklist.add("たん");
+        blacklist.add("くん");
+        blacklist.add("そっ");
+        blacklist.add("ふっ");
+        blacklist.add("ぽかっ  ");
+
     }
     private static boolean blacklisted(String test)
     {
-
-        return false;
+        return blacklist_enabled && (blacklist.contains(test.trim()));
     }
     private static String readline(InputStreamReader f)
     {
@@ -44,7 +194,6 @@ public class Main
             try
             {
                 int c = f.read();
-                //if(c < 0x20) break;
                 if(c < 0) break;
                 line += (char)c;
                 didanything = true;
@@ -57,30 +206,21 @@ public class Main
             }
         }
         if(didanything)
-        {
             return line.trim();
-        }
         else
             return null;
     }
-    private static PrintStream out;
     public static void main(String[] args) throws UnsupportedEncodingException
     {
         out = new PrintStream(System.out, true, "UTF-8");
-
-        Pattern id_re = Pattern.compile("wordId=([0-9]*)");
-        Matcher id_m = id_re.matcher("");
 
         Pattern p_re = Pattern.compile("^[\\p{Punct} 　─]*$", Pattern.UNICODE_CHARACTER_CLASS);
         Matcher p_m = p_re.matcher("");
 
         miniFrequencyData data = new miniFrequencyData();
 
-        Tokenizer tokenizer = new Tokenizer.Builder()
-            //.mode(Tokenizer.Mode.NORMAL)
-            //.kanjiPenalty(3, 10000)
-            //.otherPenalty(Integer.MAX_VALUE, 0)
-            .build();
+        init_blacklist();
+        Tokenizer tokenizer = new Tokenizer.Builder().build();
 
         InputStreamReader in;
         try
@@ -109,8 +249,8 @@ public class Main
 
                 if(!token.isKnown()) continue; // not from the dictionary
                 if(p_m.reset(token.getSurface()).find()) continue; // punctuation
-                //if(!id_m.reset(token.toString()).find()) continue; // failed to find numeric id
 
+                // not all of these work on the unidic tags, but some of them do, and the broken ones will work for the ipadic tags if you end up using ipadic for some reason
                 if(token.getPartOfSpeechLevel1().contains("助詞")) continue; // particles
                 if(token.getPartOfSpeechLevel1().contains("助動詞")) continue; // strict auxiliary verbs (ます, だ, etc)
                 if(token.getPartOfSpeechLevel1().contains("感動詞")) continue; // interjection
@@ -121,17 +261,13 @@ public class Main
                 if(token.getPartOfSpeechLevel1().contains("記号")) continue; // symbols
                 if(token.getPartOfSpeechLevel1().contains("記号")) continue; // symbols
 
-                String parts = token.getPartOfSpeechLevel1()+"\t"+token.getPartOfSpeechLevel2()+"\t"+token.getPartOfSpeechLevel3();//+"\t"+token.getPartOfSpeechLevel4() // 4 doesn't seem to be used in unidic
+                String parts = token.getPartOfSpeechLevel1()+"\t"+token.getPartOfSpeechLevel2()+"\t"+token.getPartOfSpeechLevel3();
                 if(parts.contains("形容詞	非自立")) continue; // dependent i-adjectives (いい from ていい etc)
                 if(parts.contains("形容詞	接尾")) continue; // abnormal adjectives (ぽい etc)
                 if(parts.contains("動詞	非自立")) continue; // dependent verbs (やる from てやる etc)
                 if(parts.contains("動詞	接尾")) continue; // abnormal verbs (られる etc)
 
-                // Old code to keep in VCS
-                //String id = id_m.group(1);
-                //String[] temp = {parts, token.getLemma(), token.getLemmaReadingForm(), token.getPronunciation(), token.getPronunciationBaseForm(), token.getWrittenForm(), token.getWrittenBaseForm(), token.getLanguageType()};
-                //data.addEvent(id, token.getAllFeatures());
-                //data.addEvent(id, identity);
+                if(blacklisted(token.getWrittenBaseForm())) continue;
 
                 //Kana spelling   Kana pronunciation  Actual pronunciation   Term
                 //イウ            ユウ                ユー                   いう
@@ -139,15 +275,15 @@ public class Main
                 //トウ            トウ                トウ                   問う
                 //getKanaBase(), getFormBase(), getPronunciationBaseForm(), getWrittenBaseForm()
                 //everything else is affected in conjugation
+
+                // record event
+
                 String[] temp = {parts, token.getWrittenBaseForm(), token.getFormBase(), token.getPronunciationBaseForm(), token.getAccentType(), token.getConjugationType(), token.getLanguageType()};
                 String identity = StringUtils.join(temp,"\t");
                 data.addEvent(identity);
             }
         }
         for(miniAlternativeFact fact : data.getSortedFrequencyList())
-        {
-            //out.println(fact.count+"\t"+fact.id+"\t"+fact.data);
             out.println(fact.count+"\t"+fact.id);
-        }
     }
 }
